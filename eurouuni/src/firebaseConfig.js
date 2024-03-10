8d77d9c2609851d2dc9a668ace568530f714437f
laurisandman <146067265+laurisandman@users.noreply.github.com>
2024-03-10 04:48:42 +0200

    yhteys firebase terminaalissa

--- eurouuni/src/firebaseConfig.js
+++ eurouuni/src/firebaseConfig.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
+//import { getAuth } from "firebase/compat/auth";
+//import { GoogleAuthProvider } from "firebase/auth";
+
+//const provider = new GoogleAuthProvider();
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-f1yUyy447cwj7qxCikUna512nAIzquc",
@@ -17,3 +21,14 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 export default firebaseConfig;
+
+//import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+
+//const firebaseConfig = {
+// Yours details
+//};
+
+// Initialize Firebase 
+//const app = initializeApp(firebaseConfig);
+//export const auth =  getAuth(app);
+//export const googleProvider = new GoogleAuthProvider();
