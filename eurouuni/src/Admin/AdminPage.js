ea773a42708bc1c07bbb4bcd91f3dc3765067f15
Joel Heinonen <72456705+Khcup@users.noreply.github.com>
2024-03-13 12:00:56 +0200

    Update AdminPage.js

--- eurouuni/src/Admin/AdminPage.js
+++ eurouuni/src/Admin/AdminPage.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import firebaseConfig from '../firebaseConfig';
 
 const AdminPage = () => {
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }, []);
+
+  // Function to handle logout
+  const handleLogout = async () => {
+    try {
+      await firebase.auth().signOut();
+      alert("Logout successful!");
+      return <Navigate to="/" />;
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
   return (
     <div>
       <h1>Admin Page</h1>
       {/* Add admin features here */}
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
