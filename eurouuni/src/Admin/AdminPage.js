89ec48c668a5bbb115cde6eb7da070ce05fe7001
Joel Heinonen <72456705+Khcup@users.noreply.github.com>
2024-03-14 12:22:21 +0200

    Added checks
    Fixed critical security which allowed users to acces admin page without being logged in.

--- eurouuni/src/Admin/AdminPage.js
+++ eurouuni/src/Admin/AdminPage.js
@@ -1,24 +1,34 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
+import { useNavigate } from 'react-router-dom';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import firebaseConfig from '../firebaseConfig';
 
 const AdminPage = () => {
-  const navigate = useNavigate(); // Initialize useNavigate hook
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
-  }, []);
 
-  // Function to handle logout
+    // Check authentication status when component mounts
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        // User is not logged in, redirect to login page
+        navigate('/auth');
+      }
+    });
+
+    // Clean up the subscription when component unmounts
+    return () => unsubscribe();
+  }, [navigate]);
+
   const handleLogout = async () => {
     try {
       await firebase.auth().signOut();
       alert("Logout successful!");
-      navigate('/'); // Use navigate function to redirect to home page
+      navigate('/'); // Redirect to home page after logout
     } catch (error) {
       console.error("Error logging out:", error);
     }
