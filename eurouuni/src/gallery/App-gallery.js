54159cb0af7a611fc702d926a3bf40e362eea217
Joel Heinonen <72456705+Khcup@users.noreply.github.com>
2024-03-27 11:02:00 +0200

    Update App-gallery.js

--- eurouuni/src/gallery/App-gallery.js
+++ eurouuni/src/gallery/App-gallery.js
@@ -7,16 +7,19 @@ import firebaseConfig from "../firebaseConfig";
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
+const storage = firebase.storage();
 const firestore = firebase.firestore();
 
-const AppGalleryItem = ({ item, toggleGallery, showText }) => (
-  <div className="app-gallery-item">
+const AppGalleryItem = ({ item, toggleGallery, showText}) => (
+  <div className={`app-gallery-item`}>
     <div className="item-wrapper">
-      <div onClick={() => toggleGallery(item.title)} style={{ cursor: "pointer" }}>
+      <div
+        onClick={() => toggleGallery(item.title)}
+        style={{ cursor: "pointer" }}
+      >
         <img alt={item.title} className="img-responsive" src={item.imageUrl} />
       </div>
-      {showText && <div>{item.title}</div>}
+      {showText && <div className="galleryitemtittle">{item.title}</div>}
     </div>
   </div>
 );
@@ -27,7 +30,12 @@ const GalleryCategory = ({ category, selected, toggleGallery }) => (
     <div className="category-items">
       {category.items.map((item) => (
         <div key={item.id} className="category-item">
-          <AppGalleryItem item={item} toggleGallery={toggleGallery} showText={!selected} />
+          <AppGalleryItem
+            item={item}
+            toggleGallery={toggleGallery}
+            showText={!selected}
+            inCategory={true}
+          />
         </div>
       ))}
     </div>
@@ -43,7 +51,10 @@ const Gallery = () => {
   useEffect(() => {
     const fetchDescription = async () => {
       try {
-        const descriptionDoc = await firestore.collection("descriptions").doc("tulisijat").get();
+        const descriptionDoc = await firestore
+          .collection("descriptions")
+          .doc("tulisijat")
+          .get();
         if (descriptionDoc.exists) {
           setDescription(descriptionDoc.data().text);
         } else {
@@ -58,29 +69,35 @@ const Gallery = () => {
       try {
         const categoriesRef = firestore.collection("tulisijatcategories");
         const orderRef = firestore.collection("categoryOrder").doc("order");
-    
+
         const snapshot = await categoriesRef.get();
         const orderSnapshot = await orderRef.get();
-    
+
         const fetchedCategories = await Promise.all(
           snapshot.docs.map(async (doc) => {
             const categoryId = doc.id;
             const categoryData = doc.data();
-            
+
             // Check if 'text' field exists in the document data
-            const categoryName = categoryData && categoryData.text ? categoryData.text : "";
-            
+            const categoryName = categoryData?.text || "";
+
             const categoryImages = await fetchCategoryImages(categoryId);
             return {
               id: categoryId,
               title: categoryName,
               items: categoryImages || [],
             };
-          })
+          }),
         );
 
         // Apply category order
-        const orderedCategories = orderSnapshot.exists ? orderSnapshot.data().order.map(id => fetchedCategories.find(category => category.id === id)) : fetchedCategories;
+        const orderedCategories = orderSnapshot.exists
+          ? orderSnapshot
+              .data()
+              .order.map((id) =>
+                fetchedCategories.find((category) => category.id === id),
+              )
+          : fetchedCategories;
 
         setCategories(orderedCategories);
         setLoading(false); // Set loading to false after fetching is complete
@@ -95,19 +112,40 @@ const Gallery = () => {
   }, []);
 
   const fetchCategoryImages = async (categoryId) => {
-    const storageRef = firebase.storage().ref().child("images").child(categoryId);
-    const categoryImages = await storageRef.listAll();
+    try {
+      const categoryImagesRef = storage.ref().child("images").child(categoryId);
+      const categoryImages = await categoryImagesRef.listAll();
 
-    const categoryItems = await Promise.all(
-      categoryImages.items.map(async (imageRef) => {
-        const url = await imageRef.getDownloadURL();
-        return { id: imageRef.name, title: imageRef.name, imageUrl: url };
-      })
-    );
+      // Fetch image URLs and create an array of objects with id, title, and imageUrl
+      const categoryItems = await Promise.all(
+        categoryImages.items.map(async (imageRef) => {
+          const imageUrl = await imageRef.getDownloadURL();
+          const id = imageRef.name; // Use imageRef.name as ID
+          return { id, title: imageRef.name, imageUrl };
+        }),
+      );
 
-    return categoryItems;
+      // Fetch the image order from Firestore
+      const orderSnapshot = await firestore
+        .collection("categoryImagesOrder")
+        .doc(categoryId)
+        .get();
+      const orderData = orderSnapshot.data();
+      if (orderData?.order) {
+        const orderedIds = orderData.order;
+        categoryItems.sort((a, b) => {
+          const aIndex = orderedIds.indexOf(a.imageUrl);
+          const bIndex = orderedIds.indexOf(b.imageUrl);
+          return aIndex - bIndex;
+        });
+      }
+
+      return categoryItems;
+    } catch (error) {
+      console.error("Error fetching category images:", error);
+      return [];
+    }
   };
-
   const toggleGallery = (title) => {
     if (selectedCategory && selectedCategory.title === title) {
       setSelectedCategory(null);
@@ -127,7 +165,10 @@ const Gallery = () => {
 
       {selectedCategory ? (
         <div>
-          <button className="back-button" onClick={() => setSelectedCategory(null)}>
+          <button
+            className="back-button"
+            onClick={() => setSelectedCategory(null)}
+          >
             « Back
           </button>
           <GalleryCategory
@@ -140,7 +181,11 @@ const Gallery = () => {
         categories.map((category) => (
           <AppGalleryItem
             key={category.id}
-            item={{ id: category.id, title: category.title, imageUrl: category.items[0]?.imageUrl }}
+            item={{
+              id: category.id,
+              title: category.title,
+              imageUrl: category.items[0]?.imageUrl,
+            }}
             toggleGallery={toggleGallery}
             showText={true}
           />
